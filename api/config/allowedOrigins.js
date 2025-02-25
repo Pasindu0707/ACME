91aab7fc25c8d4a1310edfefd13adb2427deb340
Pasindu0707 <123113924+Pasindu0707@users.noreply.github.com>
2025-02-25 23:13:12 +0530

    commit

--- api/config/allowedOrigins.js
+++ api/config/allowedOrigins.js
@@ -1,12 +1,12 @@
-const allowedOrigins=[
-    "http:127.0.0.1:5500",
+const allowedOrigins = [
     "http://localhost:3500",
     "http://localhost:3000",
     "https://www.google.com",
     "http://www.mysite.lk",
     "http://localhost:5173",
     "https://acme-cap7m7lhl-pasindu-fernandos-projects.vercel.app",
-    "https://acme-ochre-rho.vercel.app"
-]
+    "https://acme-ochre-rho.vercel.app",
+    "https://acme-fe-livid.vercel.app"  // Add this origin
+];
 
-export default allowedOrigins
+export default allowedOrigins;
